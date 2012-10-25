c0fe82a84c39bd40d188a379920b0a0e9b1d91a2
maxdebayser <none@none>
2012-10-25 20:41:46 -0200

    Corricao do build no windows
    Tinha uma funcao replicada no modulos para windows e para windows
    que eh independente de plataforma. Eu a movi para o fonte independente
    de plataforma mais esqueci de remover a duplicata no modulo do windows.

--- project/src/windows/BackTrace.cpp
+++ project/src/windows/BackTrace.cpp
@@ -9,17 +9,6 @@ namespace Backtrace {
         initializeExecutablePath(argv0);
 	}
 
-	StackTrace* trace()
-	{
-		const int MAX_STACK = 32;
-		std::auto_ptr<StackTrace> trace(new StackTrace());
-		trace->getFrames().resize(MAX_STACK);
-		const int num = getPlatformStackLoader().getStack(MAX_STACK, &trace->getFrames()[0]);
-		trace->getFrames().resize(num);
-
-		return trace.release();
-	}
-
 	bool backtraceSupported()
 	{
 		return true;
